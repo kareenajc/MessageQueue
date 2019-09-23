e2aa94925949d1000fda7023f6a88f9cc89c6ba1
Kareena Chan <kareena.chan@student.csulb.edu>
2019-09-23 10:33:28 -0700

    Adding base files

---
+++ ProbeB.cpp
@@ -0,0 +1,6 @@
+#include "force_patch.h" //the header file that allows ProbeB to terminate when the hub reaches 10,000 messages
+
+int main ()
+{
+	return 0;
+}
---
+++ ProbeC.cpp
@@ -0,0 +1,6 @@
+#include "kill_patch.h" //the header file that allows ProbeC to terminate when the User enters a kill command
+
+int main ()
+{
+	return 0;
+}
